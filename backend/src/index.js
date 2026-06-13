c35dd96a6a92b885d0490f00b9b3b3bdae517e13
Harryy <137473897+ekihari27@users.noreply.github.com>
2026-06-13 14:47:51 +0700

    feat: prepare app for vercel deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -229,6 +229,10 @@ app.post("/proker", async (req, res) => {
 });
 
 // start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
